dda2376bc6377961637c4e5a66bb40cf49f8b6ae
Daksh Gupta <107475244+dgupta99@users.noreply.github.com>
2024-12-05 21:47:20 +0530

    Delete task1.js

--- task1.js
+++
@@ -1,9 +0,0 @@
-const reversestring= (str)=>str.split("").reverse().join("")
-//we split the string into an array,reverse the array and join it again
-console.log(reversestring('hello')) 
-//The log() method writes (logs) a message to the console.
-
-console.log("hello world");
-
-//The changes are made in collab by akansha 
-
