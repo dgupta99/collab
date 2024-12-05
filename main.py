c13d2e24f0f5809ff7c6be3bde28ed0b96ad51cd
Daksh <daksh.dg03@gmail.com>
2024-12-05 22:00:03 +0530

    heg

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def sum_two_numbers(a, b):
 
 if __name__ == "__main__":
     greet_user()
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(input("Enter the third number: "))
+    num2 = float(input("Enter the first number: "))
     print(f"The sum of {num1} and {num2} is {sum_two_numbers(num1, num2)}.")
+    
